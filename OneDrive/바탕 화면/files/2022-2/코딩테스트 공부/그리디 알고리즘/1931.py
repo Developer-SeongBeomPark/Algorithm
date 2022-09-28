620671853bbe153dbbdef1c02de3cbbbcf05d56e
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-09-28 18:33:12 +0900

    회의실 배정

---
+++ OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/1931.py
@@ -0,0 +1,57 @@
+# # O(N) = n**2 인 경우
+
+# max_length = int(input())
+
+# array = []
+
+# for i in range(max_length):
+#     value = input()
+#     arr = list(map(int, value.split(" ")))
+#     array.append(arr)
+
+# start = array[0][0]
+# end = array[0][1]
+# count = 1
+# max_count = 1
+
+# for i in range(max_length - 1):
+#     for j in range(i+1,max_length):
+
+#         if(array[j][0] >= end):
+#             start = array[j][0]
+#             end = array[j][1]
+#             count += 1
+        
+#     if(count > max_count):
+#         max_count = count
+#         count = 1
+
+# print(max_count) # --> 시간 초과
+
+
+# O(N) =  N인 경우
+
+max_length = int(input())
+
+array = []
+
+for i in range(max_length):
+    value = input()
+    arr = list(map(int, value.split(" ")))
+    array.append(arr)
+
+array.sort(key = lambda x : (x[1],x[0])) # sort,sorted 함수의 시간 복잡도 : O(nlogn)
+
+start = array[0][0]
+end = array[0][1]
+count = 1
+
+for i in range(1,max_length):
+    if(array[i][0] >= end):
+        start = array[i][0]
+        end = array[i][1]
+        count += 1
+
+print(count)
+
+
