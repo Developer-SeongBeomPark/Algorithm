de8a2584a60b7e0926b9d5bd0fb58b233a907b9c
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-10-15 23:34:16 +0900

    강의실 배정

---
+++ OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/11000.py
@@ -0,0 +1,7 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+lecture = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+
+print(lecture)
--- OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/2437.py
+++ OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/2437.py
@@ -7,15 +7,13 @@ weights = list(map(int,sys.stdin.readline().split()))
 
 #처음부터 추 무게를 더해가며 자신의 앞에 있는 추 무게의 합보다 자신의 무게가 크면 그 사이값들은 표현할 수가 없음
 
-value = 0
+value = 1
 
 weights.sort()
 
-for i in range(1,N):
-    value += weights[i-1]
-    if(weights[i] > value):
-        value += 1
+for i in weights:
+    if(i > value):
         break
+    value += i
 
 print(value)
-
