3336e004439e0231df18b52d957537645fbcff25
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-10-18 21:47:11 +0900

    강의실 배정

--- OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/11000.py
+++ OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/11000.py
@@ -1,7 +1,40 @@
 import sys
+import heapq
 
 N = int(sys.stdin.readline())
 
 lecture = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
-print(lecture)
+tmp = []
+
+num = 0
+
+heapq.heapify(lecture)
+heapq.heapify(tmp)
+
+while lecture or tmp:
+    if(not tmp):
+        num += 1
+        [start,end] = heapq.heappop(lecture)
+        for i in range(len(lecture)):
+            value = heapq.heappop(lecture)
+            if(value[0] >= end):
+                start = value[0]
+                end = value[1]
+            else:
+                heapq.heappush(tmp,value)
+
+    elif(not lecture):
+        num += 1
+        [start,end] = heapq.heappop(tmp)
+        for i in range(len(tmp)):
+            value = heapq.heappop(tmp)
+            if(value[0] >= end):
+                start = value[0]
+                end = value[1]
+            else:
+                heapq.heappush(tmp,value)
+
+print(num)
+
+## 시간 초과
