0009ae5a2e05894db7351a65baa103dc819dc629
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-10-21 15:22:27 +0900

    주유소

--- OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/13305.py
+++ OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/13305.py
@@ -1,6 +1,4 @@
 import sys
-from xmlrpc.client import MAXINT
-
 N = int(sys.stdin.readline())
 
 distance = list(map(int,sys.stdin.readline().split()))
@@ -12,14 +10,6 @@ price_list.pop()
 min_price = price_list[0]
 dis = distance[0]
 total_price = min_price * dis
-
-# while price_list:
-#     min_price = min(price_list)
-#     min_price_index = price_list.index(min_price)
-#     part_sum = min_price * sum(distance[min_price_index:])
-#     total_price += part_sum
-#     distance = distance[:min_price_index]
-#     price_list = price_list[:min_price_index]
 
 for i in range(1, len(distance)):
     if(price_list[i] < min_price):
