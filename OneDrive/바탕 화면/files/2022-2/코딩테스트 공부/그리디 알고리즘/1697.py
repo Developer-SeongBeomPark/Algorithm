32fe81423f9a4bc87dbbc5538b3e7857ae7d12ba
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-10-27 21:13:30 +0900

    숨바꼭질

--- OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/1697.py
+++ OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/1697.py
@@ -1,9 +1,19 @@
 import sys
+from collections import deque
 
-sys.setrecursionlimit(10**6)
+def bfs(start):
+    q = deque([start])
+    while q:
+        start = q.popleft()
+        if(start == K):
+            return second_count[start]
+        for i in (start-1,start+1,start*2):
+            if(0 <= i < 100001 and not second_count[i]):
+                second_count[i] = second_count[start] + 1
+                q.append(i)
 
 N,K = map(int, sys.stdin.readline().split())
 
+second_count = [0] * 100001
 
-
-
+print(bfs(N))
