68676534e9508c11979ab20fcfb4bc4b440ca59e
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-11-02 15:51:35 +0900

    토마토

--- OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/2644.py
+++ OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/2644.py
@@ -3,7 +3,6 @@ import sys
 M,N = map(int,sys.stdin.readline().split())
 
 box = list(list(map(int,sys.stdin.readline().split())) for _ in range(N))
-visited = [[0] * M for _ in range(N)]
 
 dx = [-1,0,1,0]
 dy = [0,-1,0,1]
@@ -13,24 +12,17 @@ cnt = 0
 answer = []
 
 def bfs(x,y):
-    if(0 <= x < N and 0 <= y < M and visited[x][y] == 0):
-        visited[x][y] = 1
-        global cnt
-        cnt += 1
-        for i in range(4):
-            bfs(x+dx[i],y+dy[i])
-        return 1
+    if(0 <= x < N and 0 <= y < M):
+        if(box[x][y] == 0):
+            box[x][y] = 1
+            cnt += 1
+
 
 
 for i in range(N):
     for j in range(M):
-        cnt = 0
-        if bfs(i,j) == 1:
-            total += 1
-        if(cnt > 0):
-            answer.append(cnt)
+        if(box[i][j] == 1):
+            for k in range(4):
+                bfs(i+dx[k],j+dy[k])
 
-if(total > 1):
-    print(-1)
-else:
-    print()
+print(answer)
