f9dda649f1a4fbf9a9ae01768898d89fc04ec6b7
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-11-02 23:56:41 +0900

    토마토

--- OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/2644.py
+++ OneDrive/바탕 화면/files/2022-2/코딩테스트 공부/그리디 알고리즘/2644.py
@@ -1,28 +1,47 @@
 import sys
+from collections import deque
 
 M,N = map(int,sys.stdin.readline().split())
 
 box = list(list(map(int,sys.stdin.readline().split())) for _ in range(N))
-
 dx = [-1,0,1,0]
 dy = [0,-1,0,1]
 
-total = 0
-cnt = 0
-answer = []
-
-def bfs(x,y):
-    if(0 <= x < N and 0 <= y < M):
-        if(box[x][y] == 0):
-            box[x][y] = 1
-            cnt += 1
-
-
+q = deque([])
+answer = 0
+initial = True
 
 for i in range(N):
     for j in range(M):
-        if(box[i][j] == 1):
-            for k in range(4):
-                bfs(i+dx[k],j+dy[k])
+        if box[i][j] == 1:
+            q.append([i,j])
+        if box[i][j] == 0:
+            initial = False
 
-print(answer)
+if(initial == True):
+    print(0)
+    exit()
+
+def bfs():
+    while q:
+        [x,y] = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = x + dy[i]
+            if(0 <= nx < N and 0 <= ny < M and box[nx][ny] == 0):
+                box[nx][ny] = box[x][y] + 1
+                q.append([nx,ny])
+
+bfs()
+
+for i in range(N):
+    for j in range(M):
+        if box[i][j] == 0:
+            print(-1)
+            print(box)
+            exit()
+        if(box[i][j] > answer):
+            answer = box[i][j]
+
+
+print(answer)        
